2c0950e51039b553d22a5b1bf51b714f2a26911c
ysy_glb <ysy_glb@18388.com>
2019-10-14 21:26:27 +0800

    springboot hello2

--- src/main/java/com/springboot/controller/HellController.java
+++ src/main/java/com/springboot/controller/HellController.java
@@ -1,13 +1,10 @@
 package com.springboot.controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HellController {
 
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
