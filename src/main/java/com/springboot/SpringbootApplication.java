02126f38fa079d3bb52e7c3fba55f4881f950ac5
ysy_glb <ysy_glb@18388.com>
2019-10-14 20:36:28 +0800

    springboot hello

--- src/main/java/com/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/SpringbootApplication.java
@@ -2,10 +2,11 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//表明一个springboot程序
 @SpringBootApplication
 public class SpringbootApplication {
 
+    //启动程序
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
