ba9f6a2d35529474c5a5c2805f1b06d209ec1abd
ysy_glb <ysy_glb@18388.com>
2019-10-17 08:40:35 +0800

    springboot日志信息

--- src/test/java/com/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/springboot/SpringbootApplicationTests.java
@@ -3,6 +3,8 @@ package com.springboot;
 import com.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -19,6 +21,7 @@ public class SpringbootApplicationTests {
      Person person;
      @Autowired
     ApplicationContext ac;
+     private Logger logger = LoggerFactory.getLogger(SpringbootApplicationTests.class);
 
      @Test
      public void testHelloService(){
@@ -29,6 +32,22 @@ public class SpringbootApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(person);
+    }
+
+    //输出日志级别
+    //日志级别为:trace<debug<info<warn<error
+    /*
+      可以调整需要输出的日志级别，只输出这个级别及以后高级别生效,
+      没有指定的文件按照默认(root,就是info)输出
+     */
+    @Test
+    public void TestLogger(){
+         logger.trace("trace日志级别");
+         logger.debug("debug日志");
+         //springBoot默认日志级别
+         logger.info("info日志");
+         logger.warn("warn日志");
+         logger.error("error日志");
     }
 
 }
